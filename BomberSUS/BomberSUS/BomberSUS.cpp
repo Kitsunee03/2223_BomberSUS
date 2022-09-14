3cf4f1693eb87d9c9a0dddc4b61820c80233e2b2
Kit <arnaum03@gmail.com>
2022-09-14 20:44:29 +0200

    Level import

--- BomberSUS/BomberSUS/BomberSUS.cpp
+++ BomberSUS/BomberSUS/BomberSUS.cpp
@@ -1,28 +1,242 @@
 #include <iostream>
-#include <random> 
-#include <vector> 
 #include <string>
 #include <fstream>
+#include <vector>
+#include <map>
+//#include "raylib.h"
 
-void OpenFile() {
-	std::fstream file;
-	file.open("newlevel.sus", std::ios::in);
+using namespace std;
+
+//Control Vars
+const float m_version = 0.2f;
+
+//Game Vars 
+string m_title = "none :'(";
+int m_texturesNum = 0;
+map<string, string> m_textures;
+
+//Map
+int m_bgWidth = 0, m_bgHeight = 0;
+int m_fgWidth = 0, m_fgHeight = 0;
+int m_objWidth = 0, m_objHeight = 0;
+string** m_background;
+string** m_foreground;
+string** m_objects;
+
+void ImportFile() {
+	ifstream file("level.sus", ios::in);
 	if (!file.is_open()) {
-		std::cout << "ERROR: \"newlevel.sus\" not found" << std::endl;
+		cout << "\n\nERROR <1>: \"level.sus\" not found!\n" << endl;
 		system("pause");
-		exit(0);
+		exit(1);
 	}
-	else { std::cout << "file opened!" << std::endl; }
+	else { cout << "file opened!" << endl; }
+	string line;
 
-	std::string line;
-	std::getline(file, line);
-	std::cout << line << std::endl;
+	//Extension Control
+	getline(file, line, ';');
+	if (line != "SUS") {
+		cout << "\n\nERROR <2>: Wrong extension detected!\n" << endl;
+		system("pause");
+		exit(2);
+	}
+	cout << "\nExtension: " << line << endl;
+	//Version Control
+	getline(file, line, ';');
+	if (stof(line) < m_version) {
+		cout << "\n\nERROR <3>: Old version detected!, upgrade to " << m_version << "\n" << endl;
+		system("pause");
+		exit(3);
+	}
+	cout << "Version: " << line << endl;
+	getline(file, line); // End Line
+
+	//Title Control
+	getline(file, line, ';');
+	if (line != "TITLE") {
+		cout << "\n\nERROR <4>: Title missing!\n" << endl;
+		system("pause");
+		exit(4);
+	}
+	//Set Title
+	getline(file, line, ';');
+	m_title = line;
+	cout << "Game Title: " << m_title << endl;
+	getline(file, line); // End Line
+
+	//Textures Control
+	getline(file, line, ';');
+	if (line != "TEXTURES") {
+		cout << "\n\nERROR <5>: Textures missing!\n" << endl;
+		system("pause");
+		exit(5);
+	}
+	//Set Textures number
+	getline(file, line, ';');
+	m_texturesNum = stoi(line);
+	cout << "\nTextures to save: " << m_texturesNum << endl;
+	getline(file, line); // End Line
+
+	//Save textures
+	for (int i = 0; i < m_texturesNum; i++) {
+		getline(file, line, ';');
+		string texId = line;
+		getline(file, line, ';');
+		string texPath = line;
+		m_textures[texId] = texPath;
+
+		cout << "Texture " << texId << " -> " << texPath << endl;
+		getline(file, line); // End Line
+	}
+
+	//Background Control
+	getline(file, line, ';');
+	if (line != "BACKGROUND") {
+		cout << "\n\nERROR <6>: Background missing!\n" << endl;
+		system("pause");
+		exit(6);
+	}
+	//Set Backrgound size
+	getline(file, line, ';');
+	m_bgWidth = stoi(line);
+	getline(file, line, ';');
+	m_bgHeight = stoi(line);
+	cout << "\nBackground size: " << m_bgWidth << "x" << m_bgHeight << endl;
+	getline(file, line); // End Line
+	//Set bg level
+	m_background = new string * [m_bgHeight]; //El doble puntero crea strings de la altura del lvl
+	for (int i = 0; i < m_bgHeight; i++) {
+		m_background[i] = new string[m_bgWidth]; //El doble puntero crea en cada altura su longitud
+	}
+	for (int i = 0; i < m_bgHeight; i++) {
+		for (int j = 0; j < m_bgWidth; j++) {
+			getline(file, line, ';');
+			m_background[i][j] = line;
+			cout << m_background[i][j] << " ";
+		}
+		if (i != m_bgHeight - 1) { cout << endl; }
+		getline(file, line); // End Line
+	}
+
+	//Foreground Control
+	getline(file, line, ';');
+	if (line != "FOREGROUND") {
+		cout << "\n\nERROR <7>: Foreground missing!\n" << endl;
+		system("pause");
+		exit(7);
+	}
+	//Set Forergound size
+	getline(file, line, ';');
+	m_fgWidth = stoi(line);
+	getline(file, line, ';');
+	m_fgHeight = stoi(line);
+	cout << "\nForeground size: " << m_fgWidth << "x" << m_fgHeight << endl;
+	getline(file, line); // End Line
+	//Set fg level
+	m_foreground = new string * [m_fgHeight]; //El doble puntero crea strings de la altura del lvl
+	for (int i = 0; i < m_fgHeight; i++) {
+		m_foreground[i] = new string[m_fgWidth]; //El doble puntero crea en cada altura su longitud
+	}
+	for (int i = 0; i < m_fgHeight; i++) {
+		for (int j = 0; j < m_fgWidth; j++) {
+			getline(file, line, ';');
+			m_foreground[i][j] = line;
+			cout << m_foreground[i][j] << " ";
+		}
+		if (i != m_fgHeight - 1) { cout << endl; }
+		getline(file, line); // End Line
+	}
+
+	//Objects Control
+	getline(file, line, ';');
+	if (line != "OBJECTS") {
+		cout << "\n\nERROR <8>: Objects missing!\n" << endl;
+		system("pause");
+		exit(8);
+	}
+	//Set Objects size
+	getline(file, line, ';');
+	m_objWidth = stoi(line);
+	getline(file, line, ';');
+	m_objHeight = stoi(line);
+	cout << "\nObjects size: " << m_objWidth << "x" << m_objHeight << endl;
+	getline(file, line); // End Line
+
+	//Set Objects level
+	m_objects = new string * [m_objHeight]; //El doble puntero crea strings de la altura del lvl
+	for (int i = 0; i < m_objHeight; i++) {
+		m_objects[i] = new string[m_objWidth]; //El doble puntero crea en cada altura su longitud
+	}
+	for (int i = 0; i < m_objHeight; i++) {
+		for (int j = 0; j < m_objWidth; j++) {
+			getline(file, line, ';');
+			m_objects[i][j] = line;
+			cout << m_objects[i][j] << " ";
+		}
+		cout << endl;
+		getline(file, line); // End Line
+	}
 
 	file.close();
 }
 
-int main() {
-	OpenFile();
+int main(void) {
+	ImportFile();
 
+	/*// Initialization
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera mode");
+
+	// Define the camera to look into our 3d world
+	Camera3D camera = { 0 };
+	camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
+	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+	camera.fovy = 45.0f;                                // Camera field-of-view Y
+	camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+
+	Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+
+	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+		// TODO: Update your variables here
+		//----------------------------------------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		BeginMode3D(camera);
+
+		DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+		DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+		DrawGrid(10, 1.0f);
+
+		EndMode3D();
+
+		DrawText("Welcome to the third dimension!", 10, 40, 20, DARKGRAY);
+
+		DrawFPS(10, 10);
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	CloseWindow();        // Close window and OpenGL context
+	//----
+	*/
 	return 0;
 }
