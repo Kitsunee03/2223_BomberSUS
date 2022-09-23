571f1ebe6539b86d580eb2e4e181016e28cd5293
Kit <arnaum03@gmail.com>
2022-09-23 20:39:54 +0200

    Textures, Players and movement

--- BomberSUS/src/BomberSUS.cpp
+++ BomberSUS/src/BomberSUS.cpp
@@ -17,6 +17,7 @@ const float m_version = 0.2f;
 string m_title = "none :'(";
 int m_texturesNum = 0;
 map<string, string> m_textures;
+map<string, Texture2D> m_textures2D;
 
 //Map
 int m_bgWidth = 0, m_bgHeight = 0;
@@ -26,8 +27,15 @@ string** m_background;
 string** m_foreground;
 string** m_objects;
 
-Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
-float cubeSize[3] = { 2.0f, 2.0f, 2.0f }; //Width Height Length
+float m_marginX, m_marginZ;
+float m_offsetX, m_offsetZ;
+
+Vector3 m_drawPosition = { 0.0f, 0.0f, 0.0f };
+float cubeSize[3] = { 1.0f, 1.0f, 1.0f }; //Width Height Length
+
+//Player
+Vector3 m_player1Pos = { 0, 1, 0 };
+bool m_bombPlaced = false;
 
 void ImportFile() {
 	ifstream file("level.sus", ios::in);
@@ -186,67 +194,150 @@ void ImportFile() {
 	file.close();
 }
 
+void LoadTextures() {
+	cout << "\nTextures Loaded:" << endl;
+	map<string, string>::iterator it;
+	for (it = m_textures.begin(); it != m_textures.end();it++) {
+		cout << "\n" << it->first << ": " << it->second << endl;
+		m_textures2D[it->first]= LoadTextureFromImage(LoadImage(it->second.c_str()));
+	}
+}
+
+void DrawLevel() {
+	//Draw Background
+	for (int i = 0; i < m_bgHeight; i++) {
+		for (int j = 0; j < m_bgWidth; j++) {
+			m_drawPosition = { (float)j +(-m_offsetX+m_marginX) ,0.0f, (float)i + (-m_offsetZ + m_marginZ) };
+
+			if (m_background[i][j] != "X") {
+				DrawCubeTexture(m_textures2D[m_background[i][j]], m_drawPosition, cubeSize[0], cubeSize[1], cubeSize[2], WHITE);
+				//DrawCubeWires(m_drawPosition, cubeSize[0], cubeSize[1], cubeSize[2], BLACK);
+			}
+		}
+	}
+	//Draw Foreground
+	for (int i = 0; i < m_fgHeight; i++) {
+		for (int j = 0; j < m_fgWidth; j++) {
+			m_drawPosition = { (float)j + (-m_offsetX + m_marginX) ,1.0f, (float)i + (-m_offsetZ + m_marginZ) };
+
+			if (m_foreground[i][j] != "0") {
+				DrawCubeTexture(m_textures2D[m_foreground[i][j]], m_drawPosition, cubeSize[0], cubeSize[1], cubeSize[2], WHITE);
+				//DrawCubeWires(m_drawPosition, cubeSize[0], cubeSize[1], cubeSize[2], BLACK);
+			}
+		}
+	}
+	//Draw Grid
+	if (m_bgHeight > m_bgWidth) { DrawGrid(m_bgHeight + 1, 1.0f); }
+	else { DrawGrid(m_bgWidth + 1, 1.0f); }
+}
+void DrawObjects() {
+	for (int i = 0; i < m_objHeight; i++) {
+		for (int j = 0; j < m_objWidth; j++) {
+			m_drawPosition = { (float)j + (-m_offsetX + m_marginX) ,1.0f, (float)i + (-m_offsetZ + m_marginZ) };
+
+			if (m_objects[i][j] == "1") {
+				m_player1Pos = { (float)i ,1, (float)j };
+				DrawSphere(m_drawPosition, cubeSize[0] / 2, WHITE);
+			}
+			if (m_objects[i][j] == "2") {
+				DrawSphere(m_drawPosition, cubeSize[0] / 2, YELLOW);
+			}
+			else if (m_objects[i][j] == "3") {
+				DrawSphere(m_drawPosition, cubeSize[0] / 2, RED);
+			}
+			else if (m_objects[i][j] == "4") {
+				DrawSphere(m_drawPosition, cubeSize[0] / 2, BLUE);
+			}
+
+			if (m_objects[i][j] == "B") {
+				if (!m_bombPlaced) { DrawSphere(m_drawPosition, cubeSize[0] / 3.0f, BLACK); }
+				else { DrawCubeWires(m_drawPosition, cubeSize[0], cubeSize[1], cubeSize[2], BLACK); }
+			}
+		}
+	}
+}
+
+void PlayerMovement() {
+	//OPPOSITE AXIS
+	//HEIGHT IS X AXIS and WIDTH IS Z AXIS
+
+	//RIGHT
+	if (IsKeyPressed(KEY_RIGHT) && m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z + 1] == "0") {
+		if (!m_bombPlaced) { m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z] = "0"; }
+		m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z + 1] = "1";
+		m_bombPlaced = false;
+	}
+	//LEFT
+	else if (IsKeyPressed(KEY_LEFT) && m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z - 1] == "0") {
+		if (!m_bombPlaced) { m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z] = "0"; }
+		m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z - 1] = "1";
+		m_bombPlaced = false;
+	}
+	//UP
+	else if (IsKeyPressed(KEY_UP) && m_objects[(int)m_player1Pos.x - 1][(int)m_player1Pos.z] == "0") {
+		if (!m_bombPlaced) { m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z] = "0"; }
+		m_objects[(int)m_player1Pos.x - 1][(int)m_player1Pos.z] = "1";
+		m_bombPlaced = false;
+	}
+	//DOWN
+	else if (IsKeyPressed(KEY_DOWN) && m_objects[(int)m_player1Pos.x + 1][(int)m_player1Pos.z] == "0") {
+		if (!m_bombPlaced) { m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z] = "0"; }
+		m_objects[(int)m_player1Pos.x + 1][(int)m_player1Pos.z] = "1";
+		m_bombPlaced = false;
+	}
+}
+void BombPlacement() {
+	if (IsKeyPressed(KEY_RIGHT_CONTROL)) {
+		m_objects[(int)m_player1Pos.x][(int)m_player1Pos.z] = "B";
+		m_bombPlaced = true;
+	}
+}
+
 int main(void) {
 	ImportFile();
 
 	// Initialization
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+	const int screenWidth = 1600;
+	const int screenHeight = 900;
+
+	//Map Offsets
+	m_marginX = m_bgWidth % 2 == 0 ? .5 : 0;
+	m_marginZ = m_bgHeight % 2 == 0 ? .5 : 0;
+	m_offsetX = m_bgWidth / 2;
+	m_offsetZ = m_bgHeight / 2;
 
 	InitWindow(screenWidth, screenHeight, m_title.c_str());
+	LoadTextures();
 
-	// Define the camera to look into our 3d world
+	//Define the camera to look into our 3d world
 	Camera3D camera = { 0 };
-	camera.position = Vector3{ 0.0f, 16.0f, 16.0f };  // Camera position
-	camera.target = Vector3{ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-	camera.up = Vector3{ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-	camera.fovy = 45.0f;                                // Camera field-of-view Y
-	camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+	camera.position = { 0.0f, 10.0f, 2.0f };	//Camera position
+	camera.target = { 0.0f, 0.0f, 0.0f };		//Camera looking at point
+	camera.up = { 0.0f, 1.0f, 0.0f };			//Camera up vector (rotation towards target)
+	camera.fovy = 45.0f;						//Camera field-of-view Y
+	camera.projection = CAMERA_PERSPECTIVE;     //Camera mode type
 
-	SetTargetFPS(120);               // Set our game to run at 60 frames-per-second
-	//--------------------------------------------------------------------------------------
+	SetTargetFPS(60);               //Set our game to run at 60 frames-per-second
 
-	// Main game loop
-	while (!WindowShouldClose())    // Detect window close button or ESC key
+	//Main game loop
+	while (!WindowShouldClose())    //Detect window close button or ESC key
 	{
-		// Update
-		//----------------------------------------------------------------------------------
-		// TODO: Update your variables here
-		//----------------------------------------------------------------------------------
+		//Update
 
-		// Draw
-		//----------------------------------------------------------------------------------
+		//Variables Update
+		PlayerMovement();
+		BombPlacement();
+
+		//Draw Things
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
-
 		BeginMode3D(camera);
 
-		//Draw Background
-		for (int i = 0; i < m_bgHeight; i++) {
-			for (int j = 0; j < m_bgWidth; j++) {
-				cubePosition = { (float)j * 2 - m_bgHeight ,0.0f, (float)i * 2 - m_bgWidth };
-				if (m_background[i][j] == "C") { DrawCube(cubePosition, cubeSize[0], cubeSize[1], cubeSize[2], GREEN); }
-				if (m_background[i][j] == "V") { DrawCube(cubePosition, cubeSize[0], cubeSize[1], cubeSize[2], DARKGREEN); }
-				DrawCubeWires(cubePosition, cubeSize[0], cubeSize[1], cubeSize[2], MAROON);
-			}
-		}
-		//Draw Foreground
-		for (int i = 0; i < m_fgHeight; i++) {
-			for (int j = 0; j < m_fgWidth; j++) {
-				cubePosition = { (float)j * 2 - m_fgHeight ,2.0f, (float)i * 2 - m_fgWidth };
-				if (m_foreground[i][j] == "P") { DrawCube(cubePosition, cubeSize[0], cubeSize[1], cubeSize[2], GRAY); }
-				if (m_foreground[i][j] == "T") { DrawCube(cubePosition, cubeSize[0], cubeSize[1], cubeSize[2], BROWN); }
-				//DrawCubeWires(cubePosition, cubeSize[0], cubeSize[1], cubeSize[2], MAROON);
-			}
-		}
-
-		//Draw Grid
-		if (m_bgHeight > m_bgWidth) { DrawGrid(m_bgHeight+1, 2.0f); }
-		else { DrawGrid(m_bgWidth+1, 2.0f); }
+		DrawLevel();
+		DrawObjects();
 
 		EndMode3D();
-
 		DrawText("Bomb them!!", 10, 40, 20, DARKGRAY);
 		DrawFPS(10, 10);
 
@@ -254,9 +345,6 @@ int main(void) {
 	}
 
 	// De-Initialization
-	//--------------------------------------------------------------------------------------
-	CloseWindow();        // Close window and OpenGL context
-	//----
-
+	CloseWindow();
 	return 0;
 }
